12db6a8846cdf4504ab06e5ac02db1fded7a6a95
SughoshBhargav <sughoshsv@.com>
2023-05-13 21:04:03 +0530

    Updates

---
+++ precache-manifest.84d97e7752dce597cf3a0ab8706b8e3c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04bdfdf06f70d22fbb1d6f75b19cfff5",
+    "url": "/Counter/index.html"
+  },
+  {
+    "revision": "465b23b03010b3410c59",
+    "url": "/Counter/static/js/2.8dfa08cb.chunk.js"
+  },
+  {
+    "revision": "fd0b05ceb257ad35d928",
+    "url": "/Counter/static/js/main.c9ddcb73.chunk.js"
+  },
+  {
+    "revision": "2a493b7f7dd9226aa62c",
+    "url": "/Counter/static/js/runtime-main.5367c105.js"
+  }
+]);
